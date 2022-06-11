0d8d82b8767c15dcb6fc95f3c9c9da697010fc0e
liuyun <yunliuly@cn.ibm.com>
2022-06-11 11:48:39 +0800

    02-处理样式资源

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import sum from "./js/sum";
-import getLength from "./js/str";
+import sum from "./js/sum"
+import getLength from "./js/str"
+import './style/css/css-demo.css'
+import './style/scss/scss-demo.scss'
+import './style/scss/sass-demo.sass'
+import './style/less/less-demo.less'
+import './style/stylus/stylus-demo.styl'
 
 const result = sum(1, 2, 3, 4, 5)
 console.log(`sum result: ${result}`)
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,40 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.s[ca]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader',
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      }
+    ]
+  },
   mode: 'production'
 }
